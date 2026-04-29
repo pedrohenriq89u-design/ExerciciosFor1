2cb21ffac6c4e9a2995520e4d8c3ce424b8b02e1
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-29 17:02:55 -0300

    Implement digit sum calculation in Ex03
    Added functionality to read an integer and calculate the sum of its digits.

--- src/Ex03.java
+++ src/Ex03.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex03 {
+    static void main(){
+
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("Digite um número: ");
+        int num = sc.nextInt();
+
+        int soma = 0;
+
+        for (; num != 0; num /= 10) {
+            soma += num % 10;
+        }
+
+        IO.println("Soma dos dígitos: " + soma);
 }
+    }
